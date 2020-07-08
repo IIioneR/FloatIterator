d2d72aba3bfc664e01bedcf80ffe884db560fda9
IIioneR <IIioneR@ukr.net>
2020-07-08 14:21:54 +0300

    changed

--- Iterator.py
+++ Iterator.py
@@ -16,19 +16,32 @@ class RangeFloatIterator:
         self.counter = self.start
 
     def __next__(self):
-        if self.counter < self.end:
-            self.counter += self.step
-            return self.counter-self.step
+        if self.step > 0:
+            if self.counter < self.end:
+                self.counter += self.step
+                return self.counter - self.step
+            else:
+                raise StopIteration
         else:
-            raise StopIteration
+
+            if self.counter > self.end:
+                self.counter += self.step
+                return self.counter - self.step
+            else:
+                raise StopIteration
 
 
-assert(list(RangeFloatIterator(5)) == [0.0, 1.0, 2.0, 3.0, 4.0])
-assert(list(RangeFloatIterator(5)) == [0, 1, 2, 3, 4])
-assert(list(RangeFloatIterator(2, 5.5, 1.5)) == [2.0, 3.5, 5.0])
-assert(list(RangeFloatIterator(0, 2, 1)) == [0.0, 1.0])
-assert(list(RangeFloatIterator(1, 6)) == [1.0, 2.0, 3.0, 4.0, 5.0])
-assert(list(RangeFloatIterator(0, 0)) == [])
-assert(list(RangeFloatIterator(100, 0)) == [])
+frange = RangeFloatIterator(10, 2, -2)
 
+for i in frange:
+    print(i)
 
+assert (list(RangeFloatIterator(5)) == [0.0, 1.0, 2.0, 3.0, 4.0])
+assert (list(RangeFloatIterator(5)) == [0, 1, 2, 3, 4])
+assert (list(RangeFloatIterator(2, 5.5, 1.5)) == [2.0, 3.5, 5.0])
+assert (list(RangeFloatIterator(0, 2, 1)) == [0.0, 1.0])
+assert (list(RangeFloatIterator(1, 6)) == [1.0, 2.0, 3.0, 4.0, 5.0])
+assert (list(RangeFloatIterator(0, 0)) == [])
+assert (list(RangeFloatIterator(100, 0)) == [])
+assert (list(RangeFloatIterator(10, 2, -2)) == [10, 8, 6, 4])
+assert (list(RangeFloatIterator(2, 5.5, 1.5)) == [2, 3.5, 5])
